cb42ecc5f5412ddae474efee0d5d183a0b4bfa40
saileshkasaju <leosailesh@gmail.com>
2017-03-05 01:22:41 +0545

    use controller in component to send data to template

--- ps-movies/movie-list.component.js
+++ ps-movies/movie-list.component.js
@@ -4,6 +4,10 @@
 	var module = angular.module("psMovies");
 
 	module.component("movieList", {
-		templateUrl: "/ps-movies/movie-list.component.html"
+		templateUrl: "/ps-movies/movie-list.component.html",
+		controllerAs: "model",
+		controller: function() {
+			this.message = "Hello from a component controller!"
+		}
 	});
 }());

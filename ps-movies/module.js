3eabe75bac049e9be327cb165a28c675c236fce4
saileshkasaju <leosailesh@gmail.com>
2017-03-05 22:59:02 +0545

    change routing to use new angular component router

--- ps-movies/module.js
+++ ps-movies/module.js
@@ -1,16 +1,13 @@
 (function() {
     "use strict";
 
-    var module = angular.module("psMovies", ["ngRoute"]);
+    var module = angular.module("psMovies", ["ngComponentRouter"]);
 
-    module.config(function ($routeProvider, $locationProvider) {
-    	$locationProvider.html5Mode(true);
-    	$routeProvider
-            .when("/list", { template: "<movie-list></movie-list>"})
-            .when("/about", { template: "<app-about></app-about>"})
-            .otherwise({ redirectTo: "/list" });
+    module.config(function ($locationProvider) {
+    	$locationProvider.html5Mode(false);
     });
 
+    module.value("$routerRootComponent", "movieApp");
     module.component("appAbout", {
         template: "This is the about page"
     });

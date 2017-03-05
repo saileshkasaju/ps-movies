8a3a7c532f835e8ddf948dc8b6ce83e6fd5b5c31
saileshkasaju <leosailesh@gmail.com>
2017-03-05 23:00:06 +0545

    Updates

---
+++ ps-movies/movie-app.component.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Edge on 3/5/2017.
+ */
+(function() {
+    "use strict";
+
+    var module = angular.module("psMovies");
+
+    module.component("movieApp", {
+        templateUrl: "/ps-movies/movie-app.component.html",
+        $routeConfig: [
+            { path: "/list", component: "movieList", name: "List" },
+            { path: "/about", component: "appAbout", name: "About" },
+            { path: "/**", redirectTo: ["List"] }
+
+        ]
+    })
+}());

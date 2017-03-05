b54942383dda250af2ad8e4f8991a95c27069c50
saileshkasaju <leosailesh@gmail.com>
2017-03-06 05:30:31 +0545

    define Overview link as default sub routes

--- ps-movies/movie-list.component.js
+++ ps-movies/movie-list.component.js
@@ -21,7 +21,7 @@
         };
 
         model.goTo = function(id) {
-            model.$router.navigate(["Details", {id: id}]);
+            model.$router.navigate(["Details", {id: id}, "Overview"]);
         };
 
         model.upRating = function(movie) {

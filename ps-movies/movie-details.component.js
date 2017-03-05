8d34b96552969ea5aedfdc3e7f6c3655808036e7
saileshkasaju <leosailesh@gmail.com>
2017-03-06 00:06:07 +0545

    Updates

---
+++ ps-movies/movie-details.component.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Edge on 3/5/2017.
+ */
+(function() {
+    "use strict";
+
+    var module = angular.module("psMovies");
+
+    module.component("movieDetails", {
+        templateUrl: "/ps-movies/movie-details.component.html",
+        // $canActivate: function($timeout) {
+        //   return $timeout(function() {
+        //       return true;
+        //   }, 2000);
+        // },
+        controllerAs: "model",
+        controller: function() {
+            var model = this;
+
+            model.$routerOnActivate = function(next) {
+                model.id = next.params.id;
+            };
+        }
+    });
+}());

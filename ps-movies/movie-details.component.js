60b6b014602e19c66b3d54ef0103cb984ef31b6c
saileshkasaju <leosailesh@gmail.com>
2017-03-06 05:33:18 +0545

    Updates

--- ps-movies/movie-details.component.js
+++ ps-movies/movie-details.component.js
@@ -8,11 +8,11 @@
 
     module.component("movieDetails", {
         templateUrl: "ps-movies/movie-details.component.html",
-        // $canActivate: function($timeout) {
-        //   return $timeout(function() {
-        //       return true;
-        //   }, 2000);
-        // },
+        $routeConfig: [
+            { path: "/overview", component: "movieOverview", name: "Overview" },
+            { path: "/cast", component: "movieCast", name: "Cast" },
+            { path: "/director", component: "movieDirector", name: "Director" }
+        ],
         controllerAs: "model",
         controller: function() {
             var model = this;
@@ -21,5 +21,17 @@
                 model.id = next.params.id;
             };
         }
+    });
+
+    module.component("movieOverview", {
+        template: "This is the overview"
+    });
+
+    module.component("movieCast", {
+        template: "This is info about the cast"
+    });
+
+    module.component("movieDirector", {
+        template: "This is info about the director"
     });
 }());
